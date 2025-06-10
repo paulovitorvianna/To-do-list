e11287272b8811f32cb5ab166d0cd67c3dd2c72b
paulo9920 <paulovitorsantosvianna@gmail.com>
2025-06-10 11:53:35 -0300

    add button and input funcionality in JS

---
+++ app.js
@@ -0,0 +1,8 @@
+function addTask() {
+    const input = document.getElementById('taskInput');
+    const taskText = input.value.trim();
+        if (taskText === '') return;
+    const li = document.createElement('li');
+        li.textContent = taskText;
+     input.value = '';
+}
