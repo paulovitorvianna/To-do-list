6de0c389dc995e3d07160755d236501b75cd6c72
paulo9920 <paulovitorsantosvianna@gmail.com>
2025-06-17 19:05:43 -0300

    fix: making my li work

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 function addTask() {
-    const input = document.getElementById('taskInput');
+    const input = document.getElementById("taskInput");
     const taskText = input.value.trim();
         if (taskText === '') return;
-    const li = document.createElement('li');
+    const li = document.createElement("li");
         li.textContent = taskText;
      input.value = '';
+     document.querySelector("ul").appendChild(li)
 }
